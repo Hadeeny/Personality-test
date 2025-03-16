13baebe8fec66c30b0ac47cc6361e76c05903f94
hadeeny <segunadeniyi064@gmail.com>
2025-03-16 11:59:26 +0100

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 // import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
-
+// import og_image from '@'
 
 // const inter = Inter({ subsets: ["latin"] });
 // const poppins = Poppins({ subsets: ['latin'], weight: '500' })
@@ -12,28 +12,17 @@ export const metadata: Metadata = {
   description: `Discover which of the four types of human behaviour
    you belong to and learn how to effectively communicate
    with people in bussiness and in life`,
-   keywords: ['Personality tests', 'Sorrounded by Idiots', 'Discover my Colour'],
-   authors: [{name: 'Oluwasegun Adeniyi', url: 'segun-dev-gray.vercel.app'}],
-   creator: 'Oluwasegun Adeniyi',
-   publisher: 'Oluwasegun Adeniyi',
-   openGraph: {
-    title: 'Next.js',
-    description: 'The React Framework for the Web',
-    url: 'https://nextjs.org',
-    siteName: 'Next.js',
-    images: [
-      {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
+  keywords: ["Personality tests", "Sorrounded by Idiots", "Discover my Colour"],
+  authors: [{ name: "Oluwasegun Adeniyi", url: "segun-dev-gray.vercel.app" }],
+  creator: "Oluwasegun Adeniyi",
+  publisher: "Oluwasegun Adeniyi",
+  openGraph: {
+    title: "Sorrounded by Idiots",
+    description: `Discover which of the four types of human behaviour
+   you belong to and learn how to effectively communicate
+   with people in bussiness and in life`,
+    url: "segun-dev-gray.vercel.app",
+    siteName: "Personality Test",
   },
   //  openGraph: [],
 };
@@ -45,10 +34,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site:id" content="1467726470533754880" />
+      <meta name="twitter:creator" content="@nextjs" />
+      <meta name="twitter:creator:id" content="1467726470533754880" />
+      <meta name="twitter:title" content="Next.js" />
+      <meta
+        name="twitter:description"
+        content="The React Framework for the Web"
+      />
+      <meta property="og:title" content=""  />
+      {/* <meta property="og:image" content={og_image} /> */}
+      <meta name="twitter:image" content="https://nextjs.org/og.png" />
       <body className={`bg-black/90 text-slate-300`}>
         <Navbar />
         {children}
-
       </body>
     </html>
   );
